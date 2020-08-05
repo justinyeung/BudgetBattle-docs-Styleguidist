cdd4b101a45fa9f7523fa9c53a10e663219f9994
Justin <justingcyeung@gmail.com>
2020-08-05 00:41:38 -0400

    add docs

---
+++ precache-manifest.7a74a3eea8bdeab4ff9cc6ab6c698f83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40e264e324775ab2f11e",
+    "url": "build/2.40e264e3.js"
+  },
+  {
+    "revision": "824d6a643c09d1917870b7243a3ffdf6",
+    "url": "build/2.40e264e3.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ac15f60af0dc4d4316",
+    "url": "build/bundle.89ac15f6.js"
+  },
+  {
+    "revision": "f6cc19585bf3eab06f09",
+    "url": "build/main.f6cc1958.js"
+  },
+  {
+    "revision": "9946bb8255a7d2eb8187f8a23e5d2b74",
+    "url": "index.html"
+  },
+  {
+    "revision": "f6cc19585bf3eab06f09",
+    "url": "static/css/main.0b975348.chunk.css"
+  }
+]);
